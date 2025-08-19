fa6fc0e4942a9f1e6dd703608a21bf17ef71a540
Michael C. Rockson <michaelrockszn@gmail.com>
2025-08-19 20:44:34 +0000

    refactor(reddit): refactor reddit service functions

--- services/reddit_service.py
+++ services/reddit_service.py
@@ -4,7 +4,11 @@ from dotenv import load_dotenv
 from utils.logger import logger
 
 
-def validate_reddit_secrets(reddit_secrets):
+def validate_reddit_secrets(reddit_secrets: dict) -> bool:
+    """
+    Validate presence of Reddit API environment variables.
+    Returns True if all secrets are found, otherwise False.
+    """
     missing_keys = []
     found_keys = []
 
@@ -15,38 +19,42 @@ def validate_reddit_secrets(reddit_secrets):
             missing_keys.append(reddit_key)
             
     if missing_keys:
-        logger.error(f"Failed to load {len(missing_keys)} environment variable(s)")
+        logger.error(f"Failed to load {len(missing_keys)} Reddit environment variable(s)")
         for key in missing_keys:
-            logger.info(f"Missing environment variable: {key}")
+            logger.info(f"Missing Reddit environment variable: {key}")
         return False
     
     if found_keys:
-        logger.info(f" {len(found_keys)} reddit environment variables were loaded successfully")
+        logger.info(f" {len(found_keys)} Reddit environment variables were loaded successfully")
     
     return True
 
 
 def connect_to_reddit():
+    """
+    Connect to Reddit API using PRAW and environment variables.
+    Returns a Reddit instance if successful, otherwise None.
+    """
     load_dotenv()
-    client_id = os.getenv("REDDIT_CLIENT_ID")
-    client_secret = os.getenv("REDDIT_CLIENT_SECRET")
-    user_agent = os.getenv("REDDIT_USER_AGENT")
 
     reddit_secrets = {
-        "REDDIT_CLIENT_ID": client_id, 
-        "REDDIT_CLIENT_SECRET": client_secret, 
-        "REDDIT_USER_AGENT": user_agent
+        "REDDIT_CLIENT_ID": os.getenv("REDDIT_CLIENT_ID"),
+        "REDDIT_CLIENT_SECRET": os.getenv("REDDIT_CLIENT_SECRET"), 
+        "REDDIT_USER_AGENT": os.getenv("REDDIT_USER_AGENT")
     }
 
     if not validate_reddit_secrets(reddit_secrets):
-        logger.error("Connection to Reddit failed due to missing environment variable(s)")
+        logger.error("Connection to the Reddit API failed due to missing environment variable(s)")
         return None
         
-    reddit = praw.Reddit(
-    client_id = client_id,
-    client_secret = client_secret,
-    user_agent = user_agent
-    )
-
-    logger.info("Connection to Reddit successful!")
-    return reddit
+    try:
+        reddit = praw.Reddit(
+        client_id = reddit["REDDIT_CLIENT_ID"],
+        client_secret = reddit["REDDIT_CLIENT_SECRET"],
+        user_agent = reddit["REDDIT_USER_AGENT"]
+        )
+        logger.info("Connection to the Reddit API was successful!")
+        return reddit
+    except Exception as e:
+        logger.exception(f"Failed to initialize Reddit client: {e}")
+        return None

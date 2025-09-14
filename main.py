0ebf716e33fe26cd54254126802de5102d705c09
Michael Rockson <michaelrockszn@gmail.com>
2025-09-14 09:27:17 +0000

    feat(scraper): Add error and exception handling

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from services.scraper_service import scrape_reddit_data, store_reddit_data
 
-def main():
+def run_program():
     reddit_data = scrape_reddit_data()
     store_reddit_data(reddit_data)
 
 if __name__ == "__main__":
-    main()
+    run_program()
--- services/scraper_service.py
+++ services/scraper_service.py
@@ -1,13 +1,22 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 from domain.coordinate import RedditCoordinate
 from utils.logger import logger
 
 execute = RedditCoordinate()
 
-def scrape_reddit_data() -> Dict[str, Any]:
-    reddit_data = execute.run_reddit_scraper()
+def scrape_reddit_data() -> Dict[str, List[Any]]:
+    try:
+        reddit_data = execute.run_reddit_scraper()
+    except Exception as e:
+        logger.error(f"Error during Reddit scraping: {e}", exc_info=True)
+        reddit_data = {"posts": [], "comments": []}
     return reddit_data
 
 
-def store_reddit_data(reddit_data: Dict[str, Any]) -> None:
-    execute.run_reddit_storage(reddit_data)
+def store_reddit_data(reddit_data: Dict[str, Any]) -> bool:
+    try:
+        execute.run_reddit_storage(reddit_data)
+        return True
+    except Exception as e:
+        logger.error(f"Error during Reddit data storage: {e}", exc_info=True)
+        return False
